dac884a50f4e618ea1d38f5a6faea665085e2ab7
Supakit Thanadittagorn <supakit@riverpark.co.th>
2014-11-22 09:08:08 +0700

    Refactor grunt serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,19 +13,23 @@ module.exports = function(grunt) {
                 reporters: 'dots'
             }
         },
-        connect: {
-            server: {
-                options: {
-                    port: 3333,
-                    keepalive: true,
-                    debug: true
-                }
-            }
-        }
+        connect: serverConfig()
     });
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
     grunt.registerTask('serve', ['connect:server']);
     grunt.registerTask('default', ['karma:dev']);
+}
+
+function serverConfig() {
+    return {
+        server: {
+            options: {
+                port: 3333,
+                keepalive: true,
+                debug: true
+            }
+        }
+    };
 }
