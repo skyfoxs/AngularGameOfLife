a3a1998d7ee349fc8005296e47648765c6f7db12
Supakit Thanadittagorn <supakit@riverpark.co.th>
2014-11-22 15:49:11 +0700

    Add Gosper Glider Gun example and fix stability for robot

--- js/game/controller/main.js
+++ js/game/controller/main.js
@@ -30,6 +30,12 @@ function GameController(Universe, $interval) {
         }
     };
 
+    this.setGosperGliderGun = function() {
+        var cellList = getGosperGliderGunCellList();
+        for (var i = 0; i < cellList.length; i++)
+            this.setLiveCell(cellList[i][0], cellList[i][1]);
+    };
+
     this.reset = function() {
         this.init();
     };
@@ -39,4 +45,45 @@ function GameController(Universe, $interval) {
     };
 
     this.init();
+
+    function getGosperGliderGunCellList() {
+        return [
+            [7, 3],
+            [7, 4],
+            [8, 3],
+            [8, 4],
+            [6, 13],
+            [7, 13],
+            [8, 13],
+            [5, 14],
+            [9, 14],
+            [4, 15],
+            [4, 16],
+            [10, 15],
+            [10, 16],
+            [7, 17],
+            [5, 18],
+            [9, 18],
+            [6, 19],
+            [7, 19],
+            [8, 19],
+            [7, 20],
+            [4, 23],
+            [4, 24],
+            [5, 23],
+            [5, 24],
+            [6, 23],
+            [6, 24],
+            [3, 25],
+            [7, 25],
+            [2, 27],
+            [3, 27],
+            [7, 27],
+            [8, 27],
+            [4, 37],
+            [4, 38],
+            [5, 37],
+            [5, 38]
+        ];
+    }
 }
--- test/controller/game_controller_spec.js
+++ test/controller/game_controller_spec.js
@@ -57,4 +57,49 @@ describe("GameController", function() {
         expect($interval.cancel).toHaveBeenCalledWith(controller.interval);
         expect(controller.isRunning).toEqual(false);
     });
+
+    it("should set Gosper Glider Gun pattern when call init example", function() {
+        controller.setGosperGliderGun();
+        var expectedCell = [
+            [7, 3],
+            [7, 4],
+            [8, 3],
+            [8, 4],
+            [6, 13],
+            [7, 13],
+            [8, 13],
+            [5, 14],
+            [9, 14],
+            [4, 15],
+            [4, 16],
+            [10, 15],
+            [10, 16],
+            [7, 17],
+            [5, 18],
+            [9, 18],
+            [6, 19],
+            [7, 19],
+            [8, 19],
+            [7, 20],
+            [4, 23],
+            [4, 24],
+            [5, 23],
+            [5, 24],
+            [6, 23],
+            [6, 24],
+            [3, 25],
+            [7, 25],
+            [2, 27],
+            [3, 27],
+            [7, 27],
+            [8, 27],
+            [4, 37],
+            [4, 38],
+            [5, 37],
+            [5, 38]
+        ];
+        for (var i = 0; i < expectedCell.length; i++)
+            expect(controller.universe.cells[expectedCell[i][0]][expectedCell[i][1]]).toEqual(cell.LIVE);
+
+    });
 });
